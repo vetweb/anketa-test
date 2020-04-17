4803dfb469b3cecba8a976a92886a4692dfa06a8
idalgo <vetweba@gmail.com>
2020-04-17 15:10:27 +0300

    Add Menu brand; Add banner; Add search layout, style & js; Changed menu, content, icons, dropdown styles; Add icons;

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,6 @@
-//Slider init statistics
+let searchBtn = document.querySelector('.jsSearchBtn');
+let searcWrapper = document.querySelector('.jsSearch');
+
+searchBtn.addEventListener('click', function () {
+    searcWrapper.classList.toggle('open');
+});
