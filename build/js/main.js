27ff5c84682ef4e203e6f3c2f3b6bf3b75e5a156
idalgo <vetweba@gmail.com>
2020-04-17 15:12:30 +0300

    Add build files; Fixes js build;

---
+++ build/js/main.js
@@ -0,0 +1,6 @@
+let searchBtn = document.querySelector('.jsSearchBtn');
+let searcWrapper = document.querySelector('.jsSearch');
+
+searchBtn.addEventListener('click', function () {
+    searcWrapper.classList.toggle('open');
+});
